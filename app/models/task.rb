08c1a436c25dad9a432cd70bd3682d7916cae627
toshinobu kokubu <kokubuempire@gmail.com>
2020-11-23 18:27:48 +0900

    バリデーションのユニーク制限解除

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,4 @@
 class Task < ApplicationRecord
   validates :name, presence: true
-  validates :name, uniqueness: true
   validates :details, presence: true
-  validates :details, uniqueness: true
 end
