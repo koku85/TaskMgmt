44fc85a4a1c781ae61a278a4bb4c648aebe22609
toshinobu kokubu <kokubuempire@gmail.com>
2020-12-13 15:03:25 +0900

    一覧画面でステータスの検索ができる

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,12 +2,36 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
+    @search_params = task_search_params
+
     if params[:sort_expired]
       @tasks = Task.all.order(deadline: "DESC")
     else
       @tasks = Task.all.order(created_at: "DESC")
     end
+
+    if params[:task].present?
+      @tasks = @tasks.search_with_name(params[:task][:name])
+      @tasks = @tasks.search_with_details(params[:task][:details])
+      if params[:task][:status].present?
+        @tasks = @tasks.search_with_status(params[:task][:status])
+      end
+     # if params[:task][:label_id].present?
+     #   @tasks = @tasks.search_with_label(params[:task][:label_id])
+     # end
+    end
+   @tasks = @tasks.page(params[:page]).per(5)
   end
+
+
+
+  # def search
+  #   if params[:search].present?
+  #     @tasks = Task.where('title LIKE ?', "%#{params[:search]}%")
+  #   else
+  #     @tasks = Task.all
+  #   end
+  # end
 
   def new
     @task = Task.new
@@ -48,11 +72,14 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:name, :details, :deadline)
+    params.require(:task).permit(:name, :details, :deadline, :status)
   end
 
   def set_task
     @task = Task.find(params[:id])
   end
 
+  def task_search_params
+    params.fetch(:search, {}).permit(:name, :details, :deadline, :status)
+  end
 end
